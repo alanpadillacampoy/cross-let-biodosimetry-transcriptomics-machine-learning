aa601a2067a6fe82e7f5faa69e5a151f8a6051cd
alanpadillacampoy <alan.padilla.ca@gmail.com>
2026-07-16 16:34:44 -0700

    v4

--- 02_data_quality.R
+++ 02_data_quality.R
@@ -6,25 +6,30 @@ library(SummarizedExperiment)
 library(readr)
 library(stringr)
 library(dplyr)
-library(hgu133plus2.db)
 library(BiocManager)
 library(biomaRt)
 library(GEOquery)
 
 list_se <- DoReMiTra::list_DoReMiTra_datasets()
 
-platform <- strsplit(list_se$Dataset[12], "_")[[1]][6]
-gpl <- GEOquery::getGEO(platform)
-gpl_table <- GEOquery::Table(gpl)
-head(gpl_table)
-se1 <- DoReMiTra::get_DoReMiTra_data(list_se$Dataset[1])
+#GPL6244 = SE 17 skip it
+
+for (i in (1:35)[-17]){
+  print(i)
+  platform <- strsplit(list_se$Dataset[i], "_")[[1]][6]
+  gpl <- GEOquery::getGEO(platform)
+  gpl_table <- GEOquery::Table(gpl)
+  se1 <- DoReMiTra::get_DoReMiTra_data(list_se$Dataset[i])
+}
+
+
 expression_matrix <- as.data.frame(SummarizedExperiment::assay(se1, "exprs"))
 probes <- as.data.frame(rownames(expression_matrix))
 colnames(probes) <- "probes"
 clean_probes <- probes[!grepl("\\(", probes$probes), , drop = FALSE]
 head(clean_probes)
 new_expression_matrix <- merge(clean_probes, 
-                               gpl_table[, c("ID", "GENE", "GENE_SYMBOL")], 
+                               gpl_table[, c("ID", "GENE_SYMBOL")], 
                                by.x = "probes",
                                by.y = "ID",
                                all.x = TRUE
@@ -43,21 +48,58 @@ expression_matrix_no_NA <- expression_matrix_new[!is.na(expression_matrix_new$GE
 View(expression_matrix_no_NA)
 View(expression_matrix_new)
 
-platform <- c()
-for (i in 1:35){
-  platform <- c(platform, strsplit(list_se$Dataset[i], "_")[[1]][6])
+# platform <- c()
+# for (i in 1:35){
+#   platform <- c(platform, strsplit(list_se$Dataset[i], "_")[[1]][6])
+# }
+# platform <- levels(as.factor(platform))
+# for (i in 1:length(platform)){
+#   gpl <- GEOquery::getGEO(platform[i])
+#   gpl_table <- GEOquery::Table(gpl)
+#   print(platform[i])
+#   print(colnames(gpl_table))
+# }
+
+se17 <- assay(get_DoReMiTra_data("SE_Rouchka_2015_ExVivo_GSE64375_GPL6244"), "exprs")
+gpl <- GEOquery::getGEO("GPL6244")
+gpl_table <- GEOquery::Table(gpl)
+head(gpl_table)
+probe_ids <- rownames(se17)
+
+# Match GPL table rows to your expression data's probe order
+matched <- gpl_table[match(probe_ids, gpl_table$ID), ]
+
+gene_assignment_col <- matched$gene_assignment
+
+extract_symbol_unique <- function(x) {
+  if (is.na(x) || x == "---") return(NA)
+  entries <- strsplit(x, " /// ")[[1]]
+  symbols <- sapply(entries, function(e) {
+    parts <- strsplit(e, " // ")[[1]]
+    if (length(parts) >= 2) trimws(parts[2]) else NA
+  })
+  unique_symbols <- unique(na.omit(symbols))
+  if (length(unique_symbols) == 1) unique_symbols else NA
 }
-platform <- levels(as.factor(platform))
-for (i in 1:length(platform)){
-  gpl <- GEOquery::getGEO(platform[i])
-  gpl_table <- GEOquery::Table(gpl)
-  print(colnames(gpl_table))
+
+extract_symbol_all <- function(x) {
+  if (is.na(x) || x == "---") return(NA)
+  entries <- strsplit(x, " /// ")[[1]]
+  symbols <- sapply(entries, function(e) {
+    parts <- strsplit(e, " // ")[[1]]
+    if (length(parts) >= 2) trimws(parts[2]) else NA
+  })
+  unique_symbols <- unique(na.omit(symbols))
+  if (length(unique_symbols) == 0) NA else paste(unique_symbols, collapse = ";")
 }
 
+gene_symbol_unique <- sapply(gene_assignment_col, extract_symbol_unique)
+gene_symbol_all <- sapply(gene_assignment_col, extract_symbol_all)
 
-
-
-
+# Attach to your expression data as a data frame with the two new columns
+expr_annot <- data.frame(probe_id = probe_ids,
+                         gene_symbol_unique = gene_symbol_unique,
+                         gene_symbol_all = gene_symbol_all)
 
 
 
