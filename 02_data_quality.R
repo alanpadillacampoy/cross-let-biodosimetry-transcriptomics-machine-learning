2a27f356b959e552ff468c669a7e18e96e1cbca7
alanpadillacampoy <alan.padilla.ca@gmail.com>
2026-07-17 16:47:46 -0700

    v 5

--- 02_data_quality.R
+++ 02_data_quality.R
@@ -11,54 +11,68 @@ library(biomaRt)
 library(GEOquery)
 
 list_se <- DoReMiTra::list_DoReMiTra_datasets()
+list_problems <- c(4, 5, 14, 20, 33)
+list_super_problems <- c(16, 18)
 
 #GPL6244 = SE 17 skip it
-
+#Replaces the names of the probes with the corresponding genes being tested.----
+#Skips Data set 17 since platform GPL6244 cannot be generalized in the same way.
 for (i in (1:35)[-17]){
-  print(i)
-  platform <- strsplit(list_se$Dataset[i], "_")[[1]][6]
-  gpl <- GEOquery::getGEO(platform)
-  gpl_table <- GEOquery::Table(gpl)
-  se1 <- DoReMiTra::get_DoReMiTra_data(list_se$Dataset[i])
+ print(i)
+ #Extracts the platform from the data set, and launches a GEO query to get the
+ #platform's metadata
+ platform <- strsplit(list_se$Dataset[i], "_")[[1]][6]
+ gpl <- GEOquery::getGEO(platform)
+ gpl_table <- GEOquery::Table(gpl)
+
+ #Extracts the expression matrix from the data set
+ se1 <- DoReMiTra::get_DoReMiTra_data(list_se$Dataset[i])
+ expression_matrix <- as.data.frame(SummarizedExperiment::assay(se1, "exprs"))
+ probes <- as.data.frame(SummarizedExperiment::rownames(expression_matrix))
+ colnames(probes) <- "probes"
+ clean_probes <- probes[!grepl("\\(", probes$probes), , drop = FALSE]
+ 
+ #Selects the possible columns where the probes and symbols may be:
+ #PROBES may be in the ID column or in the NAME column
+ #The symbol is always a combination of GENE SYMBOL so we search for SYMBOL
+ gene_column <- colnames(gpl_table %>% dplyr::select(contains("symbol")))
+ id_column <- ifelse(gpl_table[colnames(gpl_table)[1]][1,1] == 1, "NAME", "ID")
+ 
+ #Annotates the probes with the corresponding genes
+ annotated_probes <- SummarizedExperiment::merge(clean_probes,
+                                                      gpl_table[, c(id_column, gene_column)],
+                                                      by.x = "probes",
+                                                      by.y = id_column,
+                                                      all.x = TRUE)
+ 
+ #Merges the annotated probes with the original expression matrix, and relocates
+ #the Gene Symbol column to the second space
+ expression_matrix_new <- SummarizedExperiment::merge(expression_matrix,
+                                                      annotated_probes,
+                                                      by.x = "row.names",
+                                                      by.y = "probes",
+                                                      all.x = TRUE)
+ expression_matrix_new <- expression_matrix_new %>%
+   dplyr::relocate(all_of(gene_column), .before = 2)
+
+ #Deletes all NAs or empty spaces in the Gene Symbol column to keep only named genes
+ rows_to_keep <- !is.na(expression_matrix_new[[gene_column]]) &
+   expression_matrix_new[[gene_column]] != "" &
+   expression_matrix_new[[gene_column]] != "-"
+ expression_matrix_new <- expression_matrix_new[rows_to_keep, ]
+ 
+ #Assigns the subset number to the new expression matrix
+ matrix_name <- paste0("expression_matrix_new_", i)
+ assign(matrix_name, expression_matrix_new)
 }
 
+for (i in (1:35)[-17]){
+ print(i)
+ a <- get(paste0("expression_matrix_new_", i))
+print(head(a[,c(1,2,3)]))
+}
 
-expression_matrix <- as.data.frame(SummarizedExperiment::assay(se1, "exprs"))
-probes <- as.data.frame(rownames(expression_matrix))
-colnames(probes) <- "probes"
-clean_probes <- probes[!grepl("\\(", probes$probes), , drop = FALSE]
-head(clean_probes)
-new_expression_matrix <- merge(clean_probes, 
-                               gpl_table[, c("ID", "GENE_SYMBOL")], 
-                               by.x = "probes",
-                               by.y = "ID",
-                               all.x = TRUE
-                               ) 
-expression_matrix_new <- merge(expression_matrix, new_expression_matrix,
-                           by.x = "row.names",
-                           by.y = "probes",
-                           all.x = TRUE)
-
-head(expression_matrix)
-expression_matrix_new <- expression_matrix_new %>% 
-  dplyr::relocate(c("GENE" , "GENE_SYMBOL"), .before = 2)
-
-expression_matrix_no_NA <- expression_matrix_new[!is.na(expression_matrix_new$GENE),]
-
-View(expression_matrix_no_NA)
-View(expression_matrix_new)
-
-# platform <- c()
-# for (i in 1:35){
-#   platform <- c(platform, strsplit(list_se$Dataset[i], "_")[[1]][6])
-# }
-# platform <- levels(as.factor(platform))
-# for (i in 1:length(platform)){
-#   gpl <- GEOquery::getGEO(platform[i])
-#   gpl_table <- GEOquery::Table(gpl)
-#   print(platform[i])
-#   print(colnames(gpl_table))
-# }
+##Deals with the SE17 problem ----
 
 se17 <- assay(get_DoReMiTra_data("SE_Rouchka_2015_ExVivo_GSE64375_GPL6244"), "exprs")
 gpl <- GEOquery::getGEO("GPL6244")
@@ -102,18 +116,9 @@ expr_annot <- data.frame(probe_id = probe_ids,
                          gene_symbol_all = gene_symbol_all)
 
 
-
-#write.csv(expression_matrix, "original.csv")
-#write.csv(expression_matrix_new, "annotated.csv")
-
-# se1 <- get_DoReMiTra_data(list_se$Dataset[1])
-# expr1 <- as.data.frame(assay(se1, "exprs"))
-# expr <- tibble::rownames_to_column(expr1, "probe_id")
-# for (i in 2:35){
-#   se2 <- get_DoReMiTra_data(list_se$Dataset[i])
-#   expr2 <- as.data.frame(assay(se2, "exprs"))
-#   expr2_rows <- tibble::rownames_to_column(expr2, "probe_id")
-#   expr <- left_join(expr, expr2_rows, by = "probe_id")
-# }
+# # Ascending order (A to Z)
+# df_sorted <- df %>% arrange(column_name)
 # 
-# write.csv(expr, "expr.csv")
+# # Descending order (Z to A)
+# df_sorted <- df %>% arrange(desc(column_name))
+
