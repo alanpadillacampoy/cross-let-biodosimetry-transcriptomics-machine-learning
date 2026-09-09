fc1d73b5e2ced17fffb4aa98921267a5ebd0a0f5
alanpadillacampoy <alan.padilla.ca@gmail.com>
2026-09-09 16:27:47 -0700

    added RF, poly, and sqrt

--- 03_linear_regression.R
+++ 03_linear_regression.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(randomForest)
+library(tibble)
 library(ranger)
+library(glmnet)
 
 list_se <- readRDS("list_se.rds")
 final_matrices <- readRDS("final_matrices.rds")
@@ -23,6 +25,32 @@ wang_training_data <- wang_training_data %>% dplyr::select(c("Dose", all_of(wang
 # Linear Regression Model
 linear_model <- lm(Dose ~ ., data = wang_training_data)
 
-set.seed(42)
+# Random Forest Regression (version with ranger)
+random_forest_model <- ranger::ranger(
+  Dose ~ .,
+  data = wang_training_data,
+  num.trees = 60,
+  seed = 42,
+  max.depth = 2,
+  splitrule = "variance"
+)
 
-# Random Forest Regression 
+# Polynomial Linear Model
+wang_poly <- wang_training_data %>% 
+  dplyr::mutate(across(2:last_col(), ~ .x^2))
+polynomial_model <- lm(Dose ~ ., data = wang_poly)
+
+# Square Root Linear Model
+wang_root <- wang_training_data %>% 
+  dplyr::mutate(across(2:last_col(), ~ sqrt(.x)))
+square_root_model <- lm(Dose ~., data = wang_root)
+
+# Overall
+
+predictions <- tibble::tibble(Dose = wang_training_data$Dose, 
+                              Linear = predict(linear_model),
+                              RandomForest = random_forest_model$predictions,
+                              Polynomial = predict(polynomial_model),
+                              SquareRoot = predict(square_root_model)
+                              )
+predictions
